caf1b17ae2e0a4a11897a10e4747aa1ddd2ea867
rizki-rianto666 <mrizkirianto116@gmail.com>
2025-06-03 00:39:03 +0700

    another-fix-1

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -16,7 +16,14 @@ document.addEventListener("DOMContentLoaded", async () => {
   updateAuthLinks();
   await app.renderPage();
 
-  await registerServiceWorker();
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+      navigator.serviceWorker
+        .register("./sw.bundle.js")
+        .then((reg) => console.log("SW registered:", reg))
+        .catch((err) => console.error("SW registration failed:", err));
+    });
+  }
 
   window.addEventListener("hashchange", async () => {
     updateAuthLinks();
